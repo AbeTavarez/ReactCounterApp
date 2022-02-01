04f959330b913465fd511a7dca4a7355d7894323
Abraham E. Tavarez <abetavarez@icloud.com>
2022-01-31 23:29:53 -0500

    done

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 class Counter extends React.Component {
   state = {
-
+    count: 0
   }
+
+  handleIncrease = () => this.setState({count: count + 1})
+  handleDecrease = () => this.setState({ count: count - 1 })
+  
   render() {
     return (
       <div>
         <h1>React Counter App</h1>
-        <h2>0</h2>
+        <h2>{ this.state.count}</h2>
 
         <div>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={this.handleDecrease}>-</button>
+          <button onClick={this.handleIncrease}>+</button>
         </div>
       </div>
     )
